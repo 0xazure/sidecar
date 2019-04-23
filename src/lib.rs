85fc2dcf8e64701777f4dc0c0e39b27026015539
0xazure <stuart.crust@gmail.com>
2019-04-23 02:23:49 -0400

    parse post data from xml file

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 use std::error;
-use std::io;
+use std::fs::File;
+use std::io::{self, BufReader};
 use std::path::PathBuf;
 use structopt::StructOpt;
+use xml::reader::{EventReader, XmlEvent};
 
 #[derive(StructOpt, Debug)]
 #[structopt(
@@ -40,8 +42,82 @@ impl Config {
     }
 }
 
+#[derive(PartialEq)]
+enum XmlTag {
+    Post,
+    Tag,
+    PhotoUrl,
+    Other,
+}
+
+#[derive(PartialEq, Debug, Default)]
+struct Post {
+    id: String,
+    extension: Option<String>,
+    tags: Vec<String>,
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn error::Error>> {
     config.exists()?;
+
+    let file = File::open(config.posts_file)?;
+    let file = BufReader::new(file);
+    let parser = EventReader::new(file);
+
+    let mut posts: Vec<Post> = Vec::new();
+
+    let mut post: Post = Default::default();
+    let mut last_opened_tag = XmlTag::Other;
+
+    for event in parser {
+        match event {
+            Err(e) => return Err(Box::new(e)),
+            Ok(XmlEvent::StartElement {
+                name, attributes, ..
+            }) => match name.local_name.as_str() {
+                "post" => {
+                    last_opened_tag = XmlTag::Post;
+                    post.id = match attributes.iter().find(|a| a.name.local_name == "id") {
+                        Some(id) => id.value.clone(),
+                        None => {
+                            return Err(Box::new(io::Error::new(
+                                io::ErrorKind::InvalidData,
+                                "Post missing required attribute 'id'",
+                            )))
+                        }
+                    }
+                }
+                "tag" => last_opened_tag = XmlTag::Tag,
+                "photo-url" => last_opened_tag = XmlTag::PhotoUrl,
+                _ => last_opened_tag = XmlTag::Other,
+            },
+            Ok(XmlEvent::EndElement { name, .. }) => match name.local_name.as_str() {
+                "post" => {
+                    posts.push(post);
+                    post = Default::default();
+                }
+                _ => {}
+            },
+            Ok(XmlEvent::Characters(chars)) => match last_opened_tag {
+                XmlTag::Tag => post.tags.push(chars),
+                XmlTag::PhotoUrl => {
+                    let mut iter = chars.rsplitn(2, '.');
+                    let after = iter.next();
+                    let before = iter.next();
+
+                    if before.is_some() {
+                        post.extension = after.map(String::from);
+                    }
+                }
+                _ => {}
+            },
+            _ => {}
+        };
+    }
+
+    for post in posts {
+        println!("{:?}", post);
+    }
 
     Ok(())
 }
